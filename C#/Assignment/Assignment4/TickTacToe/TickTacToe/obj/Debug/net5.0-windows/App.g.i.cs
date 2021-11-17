6e7c38c3c86fe90f7c35c28b6d553ef4613ba343
Majid Shakeel Shawl <dev2.renosoftwares@gmail.com>
2021-11-17 18:39:12 +0530

    Added ASP.Net Core Project Crafts App

--- C#/Assignment/Assignment4/TickTacToe/TickTacToe/obj/Debug/net5.0-windows/App.g.i.cs
+++ C#/Assignment/Assignment4/TickTacToe/TickTacToe/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace TickTacToe {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace TickTacToe {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public static void Main() {
             TickTacToe.App app = new TickTacToe.App();
             app.InitializeComponent();
